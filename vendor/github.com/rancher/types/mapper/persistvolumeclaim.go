5fa6103c66c4a15682973f0552f18308c30dee0b
loganhz <logan@rancher.com>
2020-03-23 01:02:06 +0800

    fix

--- vendor/github.com/rancher/types/mapper/persistvolumeclaim.go
+++ vendor/github.com/rancher/types/mapper/persistvolumeclaim.go
@@ -1,8 +1,10 @@
 package mapper
 
 import (
+	"encoding/json"
 	"github.com/rancher/norman/types"
 	"github.com/rancher/norman/types/values"
+	"github.com/sirupsen/logrus"
 )
 
 type PersistVolumeClaim struct {
@@ -12,9 +14,15 @@ func (p PersistVolumeClaim) FromInternal(data map[string]interface{}) {
 }
 
 func (p PersistVolumeClaim) ToInternal(data map[string]interface{}) error {
+	jsonOutput, _ := json.Marshal(data)
 	if v, ok := values.GetValue(data, "storageClassId"); ok && v == nil {
 		values.PutValue(data, "", "storageClassId")
 	}
+	jsonOutput, _ = json.Marshal(data)
+	logrus.Info("loganpvc1" + string(jsonOutput))
+	values.RemoveValue(data, "storageClassId")
+	jsonOutput, _ = json.Marshal(data)
+	logrus.Info("loganpvc2" + string(jsonOutput))
 	return nil
 }
 
